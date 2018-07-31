dc6d63b54968be4590683fe4289db07b32af72c5
Hays <stanfordhays@gmail.com>
2018-07-31 15:34:28 -0500

    changed name of form in emailform

--- src/components/PageEmailForm/PageEmailForm.jsx
+++ src/components/PageEmailForm/PageEmailForm.jsx
@@ -23,9 +23,9 @@ class PageEmailForm extends React.Component {
     return(
       <div>
         <div>
-          <form name="contact" action="#" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+          <form name="landing-page-emails" action="#" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
         		{/*Hiddin inputs necessary for use with Netlify to use free form function*/}
-            <input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="form-name" value="landing-page-emails" />
             <input type="hidden" name="bot-field" />
             <input required type="email" name="email" value={this.state.email} placeholder={"Email"} onChange={this.handleChange} />
             <input type="submit" value="Submit" />
