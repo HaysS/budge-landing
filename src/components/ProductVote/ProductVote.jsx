3b17a1845cdef790c154088e5ba4b1587b2053cb
Hays <stanfordhays@gmail.com>
2018-08-01 14:39:11 -0500

    added more options

--- src/components/ProductVote/ProductVote.jsx
+++ src/components/ProductVote/ProductVote.jsx
@@ -23,12 +23,14 @@ class ProductVote extends React.Component {
     return(
       <div>
         <div>
-          <h1 className={"title"}>What do you want to build the most?</h1>
+          <h1 className={"title"}>Which idea do you want to build the most?</h1>
           <form name="landing-page-emails" action="#" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
         		{/*Hiddin inputs necessary for use with Netlify hosting to use free form function*/}
             <input type="hidden" name="form-name" value="landing-page-emails" />
             <input type="hidden" name="bot-field" />
-            <label>Mobile app</label><input required type="radio" name="vote" value="0" />
+            <label>Mobile app 1 </label><input required type="radio" name="vote" value="1" /><br />
+            <label>Mobile app 2 </label><input required type="radio" name="vote" value="2" /><br />
+            <label>Mobile app 3 </label><input required type="radio" name="vote" value="3" /><br />
             <input type="submit" value="Submit" />
           </form>
         </div>
