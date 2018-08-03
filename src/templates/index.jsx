dddb8d04aca8887898ba5cf9b0969bfcad18ea30
Hays <stanfordhays@gmail.com>
2018-08-03 18:36:53 -0500

    removed logo in corner

--- src/templates/index.jsx
+++ src/templates/index.jsx
@@ -75,7 +75,6 @@ class IndexTemplate extends React.Component {
             {/* The big featured header */}
             <MainHeader cover={config.siteCover}>
               <MainNav overlay={config.siteCover}>
-                <BlogLogo logo={config.siteLogo} title={config.siteTitle} />
                 <MenuButton
                   navigation={config.siteNavigation}
                   onClick={this.handleOnClick}
