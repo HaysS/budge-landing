37a60dcd874edccf3f397a8b28313a9e77fca736
Hays <stanfordhays@gmail.com>
2018-08-06 15:38:03 -0500

    fixed fancy button styling for strings of text that exceed button width

--- src/components/ProductVote/ProductVote.jsx
+++ src/components/ProductVote/ProductVote.jsx
@@ -29,9 +29,9 @@ class ProductVote extends React.Component {
       		{/*Hiddin inputs necessary for use with Netlify hosting to use free form function*/}
           <input type="hidden" name="form-name" value="landing-page-emails" />
           <input type="hidden" name="bot-field" />
-          <label><input required type="radio" name="vote" value="0" /></label><FancyButton text="Rebuild Facebook From Scratch" /><br />
-          <label><input required type="radio" name="vote" value="1" /></label><FancyButton text="Create a web app to help manage money" /><br />
-          <label><input required type="radio" name="vote" value="2" /></label><FancyButton text="Build a Reddit bot" /><br />
+          <label><input required type="radio" name="vote" value="0" /></label><FancyButton text="Rebuild Facebook" /><br />
+          <label><input required type="radio" name="vote" value="1" /></label><FancyButton text="Create Money Managing App" /><br />
+          <label><input required type="radio" name="vote" value="2" /></label><FancyButton text="Build a Reddit Bot" /><br />
           <input type="submit" value="submit" />
         </form>
       </div>
