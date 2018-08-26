149db12c368fcfd3d17e0c6e781ce5f8d9ed8697
HaysS <stanfordhays@gmail.com>
2018-08-26 15:16:19 -0500

    Updated options in ProductVote

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,13 +3,13 @@ module.exports = {
   blogAuthorDir: "sample-authors", // The name of directory that contains your 'authors' folder.
   blogAuthorId: "casper", // The default and fallback author ID used for blog posts without a defined author.
   siteTitle: "ProductLife", // Site title.
-  siteTitleAlt: "ProductLife | Make Money Building Things.", // Alternative site title for SEO.
+  siteTitleAlt: "ProductLife | Build Things People Use", // Alternative site title for SEO.
   siteLogo:
     "https://haysclark.github.io/gatsby-starter-casper/logos/logo-1024.png", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
   siteUrl: "https://productlife.netlify.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
   siteDescription:
-    "Learn to Make Money Building Things.", // Website description used for RSS feeds/meta description tag.
+    "Learn How to Build Things People Want.", // Website description used for RSS feeds/meta description tag.
   siteCover:
     "#", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
   siteNavigation: true, // If navigation is enabled the Menu button will be visible
--- src/components/ProductVote/ProductVote.jsx
+++ src/components/ProductVote/ProductVote.jsx
@@ -24,18 +24,18 @@ class ProductVote extends React.Component {
 
     return(
       <div className="product-vote">
-        <h1 className="title">What do you want to do first?</h1>
+        <h1 className="title">What do you want to learn first?</h1>
 
-        {/* ------------------------------------------------------- */}
+        {/* ------------------before launch you mother fucker fuck god fucking bitch----------------------- */}
         {/* ---- REBUILD THIS FORM TO TRACK DECISIONS OF USERS ---- */}
         {/* ------------------------------------------------------- */}
         <form name="product-vote" action={() => {}} method="post" /*data-netlify="true" data-netlify-honeypot="bot-field"*/>
       		{/*Hiddin inputs necessary for use with Netlify hosting to use free form function*/}
           <input type="hidden" name="form-name" value="product-vote" />
           <input type="hidden" name="bot-field" />
-          <label><input required type="radio" name="facebook" value="0" /></label><FancyButton text="Rebuild Facebook" /><br />
-          <label><input required type="radio" name="money-app" value="1" /></label><FancyButton text="Create a Money Managing App" /><br />
-          <label><input required type="radio" name="reddit-bot" value="2" /></label><FancyButton text="Build a Reddit Bot" /><br />
+          <label><input required type="radio" name="facebook" value="0" /></label><FancyButton text="How to Start a New Product" /><br />
+          <label><input required type="radio" name="money-app" value="1" /></label><FancyButton text="How to Get More Users" /><br />
+          <label><input required type="radio" name="reddit-bot" value="2" /></label><FancyButton text="How to Reduce Your Burn-Rate" /><br />
         </form>
       </div>
     )
